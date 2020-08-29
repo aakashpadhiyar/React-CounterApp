5936eff06f181a3c0f340998185c8c69723c481f
aakashpadhiyar <aakashpadhiyar06@gmail.com>
2020-08-29 10:14:08 +0530

    Context

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
-
+import Counter from './Counter'
 
 function App() {
   return (
-    <div>
-      <button>-</button>
-      <span>0</span>
-      <button>+</button>
-
-    </div>
+    <Counter initialCount={0} />
   )
 }
 
