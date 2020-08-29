f6e70a2622f0f223b57872be747937049296c452
aakashpadhiyar <aakashpadhiyar06@gmail.com>
2020-08-29 19:16:59 +0530

    Context Theme

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Counter from './Counter'
 import CounterHooks from './CounterHooks'
+import { prettyDOM } from '@testing-library/react';
+
+export const ThemeContext = React.createContext()
 
 function App() {
   console.log('Render App')
+  const [theme, setTheme] = useState('red')
   return (
-    <>
+    <ThemeContext.Provider value={{ backgroundColor: theme }}>
       Counter
       <Counter initialCount={0} />
     Counter Hooks
       <CounterHooks initialCount={0} />
-    </>
+      <button onClick={() => setTheme(prevTheme => {
+        return prevTheme === 'red' ? 'blue' : 'red'
+      })}>Theme</button>
+    </ThemeContext.Provider>
   )
 }
 
--- src/Counter.js
+++ src/Counter.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import { ThemeContext } from './App'
 
 export default class Content extends Component {
     constructor(props) {
@@ -12,11 +12,17 @@ export default class Content extends Component {
     render() {
         console.log('Render Counter ')
         return (
-            <div>
-                <button onClick={() => this.changeCount(-1)}>-</button>
-                <span>{this.state.count}</span>
-                <button onClick={() => this.changeCount(1)}>+</button>
-            </div>
+            <ThemeContext.Consumer>
+                {style => (
+
+
+                    <div>
+                        <button style={style} onClick={() => this.changeCount(-1)}>-</button>
+                        <span>{this.state.count}</span>
+                        <button style={style} onClick={() => this.changeCount(1)}>+</button>
+                    </div>
+                )}
+            </ThemeContext.Consumer>
         )
     }
 
